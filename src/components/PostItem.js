39aea29de4fc3fd52ffdc695b77102d0bc07aea7
Adrian Daniel Abadin <aabadin@gmail.com>
2022-11-25 20:25:51 -0300

    Terminada la generacion del front ver la logica del font

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Card,
   Stack,
@@ -18,14 +18,63 @@ const ImageContainer = styled.img`
   width: 100%;
   padding: 1rem;
 `;
+function sortArray(doc) {
+  const keys = Object.keys(doc);
+  const sortedArray = [];
+  let firstValue = 0;
+  let lastValue = keys.length;
+  let keyIndex = {};
+  keys.splice(keys.indexOf("id"), 1)
+  let indexDb = keys.indexOf("title")
+  if (indexDb !== -1) {
+    sortedArray[firstValue] = "title";
+    console.log("Se elimino title ", keys.splice(indexDb, 1));
+    firstValue++;
+  }
+  indexDb = keys.indexOf("images")
+  if (indexDb !== -1) {
+    sortedArray[lastValue - 2] = "images";
+    console.log("Se Elimino images ", keys.splice(indexDb, 1));
+
+  }
+  indexDb = keys.indexOf("render")
+  if (indexDb !== -1) {
+    sortedArray[lastValue - 3] = "render";
+    keys.splice(indexDb, 1)
+  }
+  indexDb = keys.indexOf("name")
+  if (indexDb !== -1) {
+    sortedArray[firstValue] = "name";
+    console.log("Se Elimino name ", keys.splice(indexDb, 1));
+    firstValue++;
+  }
+  indexDb = keys.indexOf("position")
+  if (indexDb !== -1) {
+    sortedArray[firstValue] = "position";
+    console.log("Se elimino position ", keys.splice(indexDb, 1));
+    firstValue++;
+  }
+  indexDb = keys.indexOf("description")
+  if (indexDb !== -1) {
+    sortedArray[firstValue] = "description";
+    keys.splice(indexDb, 1);
+    firstValue++;
+  }
+  keys.sort().forEach((key) => {
+    sortedArray[firstValue] = key;
+    firstValue++;
+  });
+  console.log(sortedArray);
+  return sortedArray
+}
 function PostItem({ doc, setPosts, index, posts }) {
   const [enabled, setEnabled] = useState(false);
   const [fields, setFields] = useState([]);
+  useEffect(() => {
+    setFields(sortArray(doc))
+    console.log("keys: ", fields)
+  }, [doc])
   //debo armar funcion que me permita generar un array con el orden de los campos que yo quiero pra que se renderize de esa manera
-  const keys = Object.keys(doc);
-  const sortedArray = [];
-  keys.forEach((key) => {});
-
   return (
     <Card
       sx={{
@@ -40,6 +89,7 @@ function PostItem({ doc, setPosts, index, posts }) {
       <Stack
         direction="column"
         sx={{ justifyContent: "flex-end", width: "100%" }}
+        
       >
         <ToggleButton
           sx={{ justifyContent: "flex-end" }}
@@ -49,8 +99,8 @@ function PostItem({ doc, setPosts, index, posts }) {
           <CheckIcon />
         </ToggleButton>
       </Stack>
-      <Stack direction="column">
-        {Object.keys(doc).map((field) => {
+      <Stack id={doc.id} direction="column">
+        {fields.map((field) => {
           if (field === "render") {
             return (
               <FormControlLabel
@@ -69,6 +119,7 @@ function PostItem({ doc, setPosts, index, posts }) {
                 name={field}
                 value={doc[field]}
                 key={uuid()}
+                multiline={(field==="description") ?true:false}
               />
             );
           }
